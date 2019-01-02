e650bb3e351bfd22ab50c980bd68c88e92352562
Jaswine <Jaswine@163.com>
2019-01-02 14:17:26 +0800

    第一次提交

---
+++ src/main/java/com/jaswine/enums/MediaTypeEnum.java
@@ -0,0 +1,14 @@
+package com.jaswine.enums;
+
+
+/**
+ * 媒体类型枚举类
+ * <P>
+*      根据RFC-6838编写的MediaType枚举类
+ * </P>
+ * @author Jaswine
+ * @date 2018-12-12
+ * @version 1.0
+ */
+public class MediaTypeEnum {
+}
